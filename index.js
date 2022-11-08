f0d47986a2b3b1cc980587db7da89c980a9791fd
jinx71 <karim.sazed@gmail.com>
2022-11-08 10:53:43 +0600

    Server setup & fakedata loaded

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3001;
-
+const data = require('./fakedata.json')
 
 app.use(cors())
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send(data)
 });
-
+app.get('/home', (req, res) => {
+    res.send(data)
+});
+app.get('/services', (req, res) => {
+    res.send(data)
+});
+app.get('/services/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    console.log(id)
+    const service = data.find(a => a.id === id)
+    console.log(service)
+    res.send(service)
+});
 
 app.post('/', (req, res) => {
     res.send('Hello world')
 })
-app.listen(port, () => console.log(`Example app listening on port ${port}\!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
