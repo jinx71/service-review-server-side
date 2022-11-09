422ee1912c35b3268b3057ea4e4ccab62ff636e5
jinx71 <karim.sazed@gmail.com>
2022-11-10 01:25:23 +0600

    Comments added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Importing Moduels
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -6,9 +8,12 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors())
 app.use(express.json())
+
+// MongoDB Database Connection
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ssqjlwr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// Database CRUD Operation
 
 async function run() {
     try {
